2e4636da3eec7683d578b03aa744364f38a00277
Mandeep Dhami <dhami@noironetworks.com>
2014-04-08 01:19:31 -0700

    Added sleep between vm boot commands

--- apic/simple-test.sh
+++ apic/simple-test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-set -x
-set -e
-
 cd /opt/stack/devstack
 . openrc admin demo
-TENANT=$(keystone tenant-list | awk '/demo/ {print $2}')
 
+if nova list | grep vm001 1>/dev/null
+then
+  echo "Test already ran"
+  nova list
+  exit 1
+fi
+
+set -e
+TENANT=$(keystone tenant-list | awk '/demo/ {print $2}')
 neutron net-create --tenant-id $TENANT net001
 neutron subnet-create --tenant-id $TENANT --name subnet001 net001 10.10.1.0/24
 neutron net-create --tenant-id $TENANT net002
@@ -25,8 +30,9 @@ IMAGE=$(nova image-list | awk '/cirros-0.3.1-x86_64-uec\ / {print $2}')
 FLAVOR=m1.tiny
 
 nova boot --image $IMAGE --flavor $FLAVOR --nic net-id=$NET1 vm001
+sleep 10
 nova boot --image $IMAGE --flavor $FLAVOR --nic net-id=$NET1 vm002
+sleep 10
 nova boot --image $IMAGE --flavor $FLAVOR --nic net-id=$NET2 vm003
-nova list
 sleep 10
 nova list
