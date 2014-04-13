ce0c62ba39261e0fc7b596d6ec322743e468a648
Mandeep Dhami <dhami@noironetworks.com>
2014-04-12 18:07:56 -0700

    Added security groups rules to allow icmp from all

--- apic/simple-test.sh
+++ apic/simple-test.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+set -e
 cd /opt/stack/devstack
 . openrc admin demo
 
 if nova list | grep vm001 1>/dev/null
 then
-  echo "Test already ran"
+  echo "Test already running"
   nova list
   exit 1
 fi
 
-set -e
 TENANT=$(keystone tenant-list | awk '/demo/ {print $2}')
 neutron net-create --tenant-id $TENANT net001
 neutron subnet-create --tenant-id $TENANT --name subnet001 net001 10.10.1.0/24
 neutron net-create --tenant-id $TENANT net002
 neutron subnet-create --tenant-id $TENANT --name subnet002 net002 10.10.2.0/24
 neutron net-list
+
+RULEID=$(neutron security-group-list -c id -c tenant_id |  awk "/$TENANT/ {print \$2}")
+neutron security-group-rule-create --protocol icmp --direction ingress $RULEID
+neutron security-group-rule-create --protocol tcp --port-range-min 22 --port-range-max 22 --direction ingress $RULEID
 
 neutron router-create --tenant-id $TENANT router001
 neutron router-interface-add router001 subnet001
