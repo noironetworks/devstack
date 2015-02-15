abe2ea78073e04ccf2f09b0dfd40e8b6952cc4e3
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-02-15 04:58:12 -0800

    Refactoring aci templates
    And adding some new templates this mostly a complete
    rewrite.

--- gbp-templates/aci/demo.sh
+++ gbp-templates/aci/demo.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 # **demo.sh**
+# Prior to running this script check the following:
+# 1. Set the name and path of the keystone_admin file in demo.conf OPENSTACK_ENV_FILE var
+# 2. The script assumes two users with the names: admin and demo
+# 3. The script requires three tenants to be present, their names are set in
+#    demo.conf as the following variables:
+#    ADMIN_TENANT_NAME, HR_TENANT_NAME, FINANCE_TENANT_NAME
 # Usage:
-# ./demo api
-# or
-# ./demo neutron
+# ./demo aci or ./demo acix or ./demo neutron
+# Use the "acix" mode for the inserting the transparent "FW + IDS" chain
 
 EXT_POLICY_NAME="Outside"
 
@@ -15,12 +20,15 @@ PORT_ADDRESS_TRANSLATION=False
 
 ACTIVE_TIMEOUT=120
 
-EXTERNAL_STACK="External"
-IT_STACK="IT"
-HR_STACK="HR-Three-Tier"
-FINANCE_STACK="Finance-Three-Tier"
+CONTRACTS_YAML="contracts.yaml"
+CONTRACTS_STACK="Contracts"
+ADMIN_YAML="admin.yaml"
+ADMIN_STACK="Admin"
+APP_YAML="app.yaml"
+HR_STACK="HR_Three_Tier"
+FINANCE_STACK="Finance_Three_Tier"
 
-USAGE="$0 <aci|neutron>"
+USAGE="$0 <aci|acix|neutron>"
 
 function set_user_password_tenant {
     local xtrace=$(set +o | grep xtrace)
@@ -71,38 +79,59 @@ set -o errexit
 set -o xtrace
 source $OPENSTACK_ENV_FILE
 
-#set_user_password_tenant $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_TENANT_NAME
+if [ -z "$ADMIN_PASSWORD" ]; then
+    ADMIN_PASSWORD=$OS_PASSWORD
+fi
+if [ -z "$NON_ADMIN_PASSWORD" ]; then
+    NON_ADMIN_PASSWORD=$OS_PASSWORD
+fi
 
-# Print the commands being run so that we can see the command that triggers
-# an error.  It is also useful for following allowing as the install occurs.
+set_user_password_tenant $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_TENANT_NAME
 
-heat stack-create -f external_connectivity.yaml "$EXTERNAL_STACK" -P "external_network_cidr=$EXT_NET_CIDR;external_network_gateway=$EXT_NET_GATEWAY;external_segment_name=$EXT_SEGMENT_NAME;physical_network_name=$PHYSICAL_NETWORK_NAME;physical_network_type=$PHYSICAL_NETWORK_TYPE;port_address_translation=$PORT_ADDRESS_TRANSLATION;mgmt_ip_pool=$SERVICES_MGMT_IP_POOL;service_management_ptg_name=$SERVICE_MANAGEMENT_PTG_NAME"
+heat stack-create -f "$CONTRACTS_YAML" "$CONTRACTS_STACK" -P "monitoring_rule_set_name=$MONITORING_RULE_SET_NAME;monitoring_to_outside_rule_set_name=$MONITORING_TO_OUTSIDE_RULE_SET_NAME;app_to_outside_rule_set_name=$APP_TO_OUTSIDE_RULE_SET_NAME;mysql_rule_set_name=$MYSQL_RULE_SET_NAME;mysql_via_fw_ids_rule_set_name=$MYSQL_VIA_FW_IDS_RULE_SET_NAME;app_rule_set_name=$APP_RULE_SET_NAME;web_tier_consumed_prs_name=$WEB_TIER_CONSUMED_PRS_NAME;web_tier_provided_prs_name=$WEB_TIER_PROVIDED_PRS_NAME"
 
-confirm_resource_created "heat stack-show" "$EXTERNAL_STACK" "CREATE_COMPLETE"
-confirm_resource_created "gbp external-segment-show" "$EXT_SEGMENT_NAME" "id"
-EXT_SEGMENT_ID=`gbp external-segment-show "$EXT_SEGMENT_NAME" | grep ' id ' | cut -d"|" -f3 | sed 's/ //g'`
+confirm_resource_created "heat stack-show" "$CONTRACTS_STACK" "CREATE_COMPLETE"
+VIP_IP_POLICY_ID=`heat output-show "$CONTRACTS_STACK" "vip_ip_policy_id" | sed "s/\"//g"`
+HTTP_WITH_LB_REDIRECT_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "http_with_lb_redirect_rule_set_id" | sed "s/\"//g"`
+APP_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "app_rule_set_id" | sed "s/\"//g"`
+MYSQL_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "mysql_rule_set_id" | sed "s/\"//g"`
+MYSQL_VIA_FW_IDS_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "mysql_via_fw_ids_rule_set_id" | sed "s/\"//g"`
+APP_TO_OUTSIDE_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "app_to_outside_rule_set_id" | sed "s/\"//g"`
+MONITORING_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "monitoring_rule_set_id" | sed "s/\"//g"`
+MONITORING_TO_OUTSIDE_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "monitoring_to_outside_rule_set_id" | sed "s/\"//g"`
 
-#confirm_resource_created "gbp group-show" "$SERVICE_MANAGEMENT_PTG_NAME" "id"
-SERVICE_MANAGEMENT_PTG_ID=`gbp group-show "$SERVICE_MANAGEMENT_PTG_NAME" | grep ' id ' | cut -d"|" -f3 | sed 's/ //g'`
+heat stack-create -f "$ADMIN_YAML" "$ADMIN_STACK" -P "infra_external_segment_name=$INFRA_EXTERNAL_SEGMENT_NAME;infra_external_policy_name=$INFRA_EXTERNAL_POLICY_NAME;app_external_segment_name=$APP_EXTERNAL_SEGMENT_NAME;app_external_policy_name=$APP_EXTERNAL_POLICY_NAME;port_address_translation=$PORT_ADDRESS_TRANSLATION;infra_ip_pool=$INFRA_IP_POOL;app_ip_pool=$APP_IP_POOL;monitoring_rule_set_id=$MONITORING_RULE_SET_ID;monitoring_to_outside_rule_set_id=$MONITORING_TO_OUTSIDE_RULE_SET_ID;app_to_outside_rule_set_id=$APP_TO_OUTSIDE_RULE_SET_ID;mysql_rule_set_id=$MYSQL_RULE_SET_ID;app_rule_set_id=$APP_RULE_SET_ID;web_tier_consumed_prs_id=$APP_TO_OUTSIDE_RULE_SET_ID;web_tier_provided_prs_id=$HTTP_WITH_LB_REDIRECT_RULE_SET_ID;monitoring_ptg_name=$MONITORING_PTG_NAME;service_management_ptg_name=$SERVICE_MANAGEMENT_PTG_NAME;management_ptg_name=$MANAGEMENT_PTG_NAME;monitoring_vm_image=$MONITORING_VM_IMAGE_NAME;monitoring_vm_flavor=$MONITORING_VM_FLAVOR;mgmt_vm_image=$MGMT_VM_IMAGE_NAME;mgmt_vm_flavor=$MGMT_VM_FLAVOR;svc_mgmt_vm_image=$SVC_MGMT_VM_IMAGE_NAME;svc_mgmt_vm_flavor=$SVC_MGMT_VM_FLAVOR;infra_l3_policy_name=$INFRA_L3_POLICY_NAME;app_l3_policy_name=$APP_L3_POLICY_NAME"
 
-set_user_password_tenant $NON_ADMIN_USERNAME $NON_ADMIN_PASSWORD $IT_TENANT_NAME
- 
-heat stack-create -f it.yaml "$IT_STACK" -P "monitoring_vm_image=$MONITORING_VM_IMAGE_NAME;monitoring_vm_flavor=$MONITORING_VM_FLAVOR;monitoring_ip_pool=$MONITORING_IP_POOL;monitoring_rule_set_name=$MONITORING_RULE_SET_NAME"
+confirm_resource_created "heat stack-show" "$ADMIN_STACK" "CREATE_COMPLETE"
+APP_L3_POLICY_ID=`heat output-show "$ADMIN_STACK" "app_l3_policy_id" | sed "s/\"//g"`
+MGMT_PTG_ID=`heat output-show "$ADMIN_STACK" "mgmt_ptg_id" | sed "s/\"//g"`
+SVC_MGMT_PTG_ID=`heat output-show "$ADMIN_STACK" "svc_mgmt_ptg_id" | sed "s/\"//g"`
+INFRA_EXTERNAL_SEGMENT_ID=`heat output-show "$ADMIN_STACK" "infra_external_segment_id" | sed "s/\"//g"`
+INFRA_EXTERNAL_POLICY_ID=`heat output-show "$ADMIN_STACK" "infra_external_policy_id" | sed "s/\"//g"`
+APP_EXTERNAL_SEGMENT_ID=`heat output-show "$ADMIN_STACK" "app_external_segment_id" | sed "s/\"//g"`
+APP_EXTERNAL_POLICY_ID=`heat output-show "$ADMIN_STACK" "app_external_policy_id" | sed "s/\"//g"`
+INFRA_L3_POLICY_ID=`heat output-show "$ADMIN_STACK" "infra_l3_policy_id" | sed "s/\"//g"`
+APP_L3_POLICY_ID=`heat output-show "$ADMIN_STACK" "app_l3_policy_id" | sed "s/\"//g"`
 
-confirm_resource_created "heat stack-show" "$IT_STACK" "CREATE_COMPLETE"
-confirm_resource_created "gbp policy-rule-set-show" "$MONITORING_RULE_SET_NAME" "id"
-MONTORING_PRS_ID=`gbp policy-rule-set-show "$MONITORING_RULE_SET_NAME" | grep ' id ' | cut -d"|" -f3 | sed 's/ //g'`
+gbp external-policy-update $INFRA_EXTERNAL_POLICY_ID --external-segments "$INFRA_EXTERNAL_SEGMENT_ID"
+gbp l3policy-update $INFRA_L3_POLICY_ID --external-segment "$INFRA_EXTERNAL_SEGMENT_ID="
+
+gbp external-policy-update $APP_EXTERNAL_POLICY_ID --external-segments "$APP_EXTERNAL_SEGMENT_ID"
+gbp l3policy-update $APP_L3_POLICY_ID --external-segment "$APP_EXTERNAL_SEGMENT_ID="
 
 set_user_password_tenant $NON_ADMIN_USERNAME $NON_ADMIN_PASSWORD $HR_TENANT_NAME
  
-heat stack-create -f three_tier_with_lb.yaml "$HR_STACK" -P "monitoring_rule_set=$MONITORING_PRS_ID;web_vm_image=$WEB_VM_IMAGE_NAME;web_vm_flavor=$WEB_VM_FLAVOR;app_vm_image=$APP_VM_IMAGE_NAME;app_vm_flavor=$APP_VM_FLAVOR;db_vm_image=$DB_VM_IMAGE_NAME;db_vm_flavor=$DB_VM_FLAVOR;app_ip_pool=$APP_IP_POOL;external_policy_name=$EXT_POLICY_NAME;external_segment_id=$EXT_SEGMENT_ID;web_tier_consumed_prs_name=$WEB_TIER_CONSUMED_PRS_NAME;web_tier_provided_prs_name=$WEB_TIER_PROVIDED_PRS_NAME"
+heat stack-create -f "$APP_YAML" "$HR_STACK" -P "web_vm_image=$WEB_VM_IMAGE_NAME;web_vm_flavor=$WEB_VM_FLAVOR;app_vm_image=$APP_VM_IMAGE_NAME;app_vm_flavor=$APP_VM_FLAVOR;db_vm_image=$DB_VM_IMAGE_NAME;db_vm_flavor=$DB_VM_FLAVOR;mysql_rule_set_id=$MYSQL_RULE_SET_ID;app_rule_set_id=$APP_RULE_SET_ID;web_tier_consumed_prs_id=$APP_TO_OUTSIDE_RULE_SET_ID;web_tier_provided_prs_id=$HTTP_WITH_LB_REDIRECT_RULE_SET_ID;vip_ip_policy_id=$VIP_IP_POLICY_ID;app_l3_policy_id=$APP_L3_POLICY_ID;mgmt_ptg_id=$MGMT_PTG_ID"
 
-confirm_resource_created "heat stack-show" "$HR_STACK" "CREATE_COMPLETE"
-confirm_resource_created "gbp policy-rule-set show" "$WEB_TIER_CONSUMED_PRS_NAME"
-gbp external-policy-update $EXT_POLICY_NAME --provided-policy-rule-sets "$WEB_TIER_CONSUMED_PRS_NAME=true"
+if [ "$RENDERING_MODE" == "acix" ]; then
+    confirm_resource_created "heat stack-show" "$HR_STACK" "CREATE_COMPLETE"
+    DB_PTG_ID=`heat output-show "$HR_STACK" "db_ptg_id" | sed "s/\"//g"`
+    APP_PTG_ID=`heat output-show "$HR_STACK" "app_ptg_id" | sed "s/\"//g"`
+    gbp policy-rule-set-update $DB_PTG_ID --provided-policy-rule-sets "$MYSQL_VIA_FW_IDS_RULE_SET_ID=true"
+    gbp policy-rule-set-update $APP_PTG_ID --consumed-policy-rule-sets "$MYSQL_VIA_FW_IDS_RULE_SET_ID=true"
+fi
 
-confirm_resource_created "gbp policy-rule-set show" "$WEB_TIER_PROVIDED_PRS_NAME"
-gbp external-policy-update $EXT_POLICY_NAME --consumed-policy-rule-sets "$WEB_TIER_PROVIDED_PRS_NAME=true"
+exit
 
 echo "*********************************************************************"
 echo "SUCCESS: End $0"
