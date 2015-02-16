50527fc8abdad96b3bbf83b7f891b1a84c58d215
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-02-16 03:01:40 -0800

    Fixing mutliple issues with demo script for launching service chain

--- gbp-templates/aci/demo.sh
+++ gbp-templates/aci/demo.sh
@@ -8,13 +8,12 @@
 #    demo.conf as the following variables:
 #    ADMIN_TENANT_NAME, HR_TENANT_NAME, FINANCE_TENANT_NAME
 # Usage:
-# ./demo aci or ./demo acix or ./demo neutron
-# Use the "acix" mode for the inserting the transparent "FW + IDS" chain
+# ./demo aci or ./demo acix
+# Use the "acix" mode for inserting the transparent "FW + IDS" chain
 
 
 ACI_RENDERING="aci"
 ACI_TRANSPARENT_SVC_RENDERING="acix"
-NEUTRON_RENDERING="neutron"
 
 PORT_ADDRESS_TRANSLATION=False
 
@@ -29,7 +28,7 @@ HR_STACK="HR_Three_Tier"
 FINANCE_STACK="Finance_Three_Tier"
 EXT_POLICY_NAME="Outside"
 
-USAGE="$0 <aci|acix|neutron>"
+USAGE="$0 <aci|acix>"
 
 function set_user_password_tenant {
     local xtrace=$(set +o | grep xtrace)
@@ -58,10 +57,6 @@ if [ "${RENDERING_MODE}"x = ""x ] ; then
     echo "USAGE: $USAGE" 2>&1
     echo "  >  No rendering mode specified." 1>&2
     exit 1
-fi
-
-if [ "$1" = "$NEUTRON_RENDERING" ] ; then
-    PORT_ADDRESS_TRANSLATION=True
 fi
 
 source demo.conf
@@ -117,22 +112,31 @@ APP_L3_POLICY_ID=`heat output-show "$ADMIN_STACK" "app_l3_policy_id" | sed "s/\"
 gbp external-policy-update $INFRA_EXTERNAL_POLICY_ID --external-segments "$INFRA_EXTERNAL_SEGMENT_ID"
 gbp l3policy-update $INFRA_L3_POLICY_ID --external-segment "$INFRA_EXTERNAL_SEGMENT_ID="
 
-gbp external-policy-update $APP_EXTERNAL_POLICY_ID --external-segments "$APP_EXTERNAL_SEGMENT_ID"
 gbp l3policy-update $APP_L3_POLICY_ID --external-segment "$APP_EXTERNAL_SEGMENT_ID="
+gbp external-policy-update $APP_EXTERNAL_POLICY_ID --external-segments "$APP_EXTERNAL_SEGMENT_ID"
 
 set_user_password_tenant $NON_ADMIN_USERNAME $NON_ADMIN_PASSWORD $HR_TENANT_NAME
  
 heat stack-create -f "$APP_YAML" "$HR_STACK" -P "web_vm_image=$WEB_VM_IMAGE_NAME;web_vm_flavor=$WEB_VM_FLAVOR;app_vm_image=$APP_VM_IMAGE_NAME;app_vm_flavor=$APP_VM_FLAVOR;db_vm_image=$DB_VM_IMAGE_NAME;db_vm_flavor=$DB_VM_FLAVOR;mysql_rule_set_id=$MYSQL_RULE_SET_ID;app_rule_set_id=$APP_RULE_SET_ID;web_tier_consumed_prs_id=$APP_TO_OUTSIDE_RULE_SET_ID;web_tier_provided_prs_id=$HTTP_WITH_LB_REDIRECT_RULE_SET_ID;vip_ip_policy_id=$VIP_IP_POLICY_ID;app_l3_policy_id=$APP_L3_POLICY_ID;mgmt_ptg_id=$MGMT_PTG_ID"
 
+confirm_resource_created "heat stack-show" "$HR_STACK" "CREATE_COMPLETE"
+
+WEB_PTG_ID=`heat output-show "$HR_STACK" "web_ptg_id" | sed "s/\"//g"`
+gbp group-update $WEB_PTG_ID --provided-policy-rule-sets "$HTTP_WITH_LB_REDIRECT_RULE_SET_ID=true"
+
+set_user_password_tenant $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_TENANT_NAME
+gbp external-policy-update $APP_EXTERNAL_POLICY_ID --consumed-policy-rule-sets "$HTTP_WITH_LB_REDIRECT_RULE_SET_ID=true"
+
+set_user_password_tenant $NON_ADMIN_USERNAME $NON_ADMIN_PASSWORD $HR_TENANT_NAME
 if [ "$RENDERING_MODE" == "$ACI_TRANSPARENT_SVC_RENDERING" ]; then
-    confirm_resource_created "heat stack-show" "$HR_STACK" "CREATE_COMPLETE"
     APP_L2_POLICY_ID=`heat output-show "$HR_STACK" "app_l2_policy_id" | sed "s/\"//g"`
     DB_PTG_ID=`heat output-show "$HR_STACK" "db_ptg_id" | sed "s/\"//g"`
     APP_PTG_ID=`heat output-show "$HR_STACK" "app_ptg_id" | sed "s/\"//g"`
     gbp group-update $APP_L2_POLICY_ID --allow-broadcast True
-    gbp policy-rule-set-update $DB_PTG_ID --provided-policy-rule-sets "$MYSQL_VIA_FW_IDS_RULE_SET_ID=true"
-    gbp policy-rule-set-update $APP_PTG_ID --consumed-policy-rule-sets "$MYSQL_VIA_FW_IDS_RULE_SET_ID=true"
+    gbp group-update $DB_PTG_ID --provided-policy-rule-sets "$MYSQL_VIA_FW_IDS_RULE_SET_ID=true"
+    gbp group-update $APP_PTG_ID --consumed-policy-rule-sets "$MYSQL_VIA_FW_IDS_RULE_SET_ID=true"
 fi
+
 
 echo "*********************************************************************"
 echo "SUCCESS: End $0"
