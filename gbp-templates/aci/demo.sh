d83563703ed0ea757507b94c9da24145e6c9fdf1
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-02-17 14:00:31 -0800

    Update demo script to add an Engineering tenant
    Also did some more refactoring of the script and the
    templates.

--- gbp-templates/aci/demo.sh
+++ gbp-templates/aci/demo.sh
@@ -17,39 +17,16 @@ ACI_TRANSPARENT_SVC_RENDERING="acix"
 
 PORT_ADDRESS_TRANSLATION=False
 
-ACTIVE_TIMEOUT=120
-
 CONTRACTS_YAML="contracts.yaml"
 CONTRACTS_STACK="Contracts"
 ADMIN_YAML="admin.yaml"
 ADMIN_STACK="Admin"
 APP_YAML="app.yaml"
 HR_STACK="HR_Three_Tier"
-FINANCE_STACK="Finance_Three_Tier"
+ENG_STACK="Eng_Three_Tier"
 EXT_POLICY_NAME="Outside"
 
 USAGE="$0 <aci|acix>"
-
-function set_user_password_tenant {
-    local xtrace=$(set +o | grep xtrace)
-    set +o xtrace
-    export OS_USERNAME=$1
-    export OS_PASSWORD=$2
-    export OS_TENANT_NAME=$3
-    export PS1='[\u@\h \W(keystone_$1)]\$ '
-    $xtrace
-}
-
-function confirm_resource_created {
-    local xtrace=$(set +o | grep xtrace)
-    set +o xtrace
-    if ! timeout $ACTIVE_TIMEOUT sh -c "while ! $1 \"$2\" | grep \"$3\"; do sleep 1; done"; then
-        set -o xtrace
-        echo "resource '$1 $2' did not become active!"
-        false
-    fi
-    $xtrace
-}
 
 # Process args
 RENDERING_MODE=$1
@@ -60,6 +37,7 @@ if [ "${RENDERING_MODE}"x = ""x ] ; then
 fi
 
 source demo.conf
+source functions-common
 
 echo "*********************************************************************"
 echo "GBP demo: $1 rendering"
@@ -84,10 +62,11 @@ fi
 
 set_user_password_tenant $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_TENANT_NAME
 
-heat stack-create -f "$CONTRACTS_YAML" "$CONTRACTS_STACK" -P "monitoring_rule_set_name=$MONITORING_RULE_SET_NAME;monitoring_to_outside_rule_set_name=$MONITORING_TO_OUTSIDE_RULE_SET_NAME;app_to_outside_rule_set_name=$APP_TO_OUTSIDE_RULE_SET_NAME;mysql_rule_set_name=$MYSQL_RULE_SET_NAME;mysql_via_fw_ids_rule_set_name=$MYSQL_VIA_FW_IDS_RULE_SET_NAME;app_rule_set_name=$APP_RULE_SET_NAME;web_tier_consumed_prs_name=$WEB_TIER_CONSUMED_PRS_NAME;web_tier_provided_prs_name=$WEB_TIER_PROVIDED_PRS_NAME"
+heat stack-create -f "$CONTRACTS_YAML" "$CONTRACTS_STACK" -P "monitoring_rule_set_name=$MONITORING_RULE_SET_NAME;monitoring_to_outside_rule_set_name=$MONITORING_TO_OUTSIDE_RULE_SET_NAME;app_to_outside_rule_set_name=$APP_TO_OUTSIDE_RULE_SET_NAME;mysql_rule_set_name=$MYSQL_RULE_SET_NAME;mysql_via_fw_ids_rule_set_name=$MYSQL_VIA_FW_IDS_RULE_SET_NAME;app_rule_set_name=$APP_RULE_SET_NAME;http_rule_set_name=$HTTP_RULE_SET_NAME;web_tier_consumed_prs_name=$WEB_TIER_CONSUMED_PRS_NAME;web_tier_provided_prs_name=$WEB_TIER_PROVIDED_PRS_NAME"
 
 confirm_resource_created "heat stack-show" "$CONTRACTS_STACK" "CREATE_COMPLETE"
 VIP_IP_POLICY_ID=`heat output-show "$CONTRACTS_STACK" "vip_ip_policy_id" | sed "s/\"//g"`
+HTTP_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "http_rule_set_id" | sed "s/\"//g"`
 HTTP_WITH_LB_REDIRECT_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "http_with_lb_redirect_rule_set_id" | sed "s/\"//g"`
 APP_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "app_rule_set_id" | sed "s/\"//g"`
 MYSQL_RULE_SET_ID=`heat output-show "$CONTRACTS_STACK" "mysql_rule_set_id" | sed "s/\"//g"`
@@ -116,14 +95,32 @@ gbp l3policy-update $INFRA_L3_POLICY_ID --external-segment "$INFRA_EXTERNAL_SEGM
 gbp l3policy-update $APP_L3_POLICY_ID --external-segment "$APP_EXTERNAL_SEGMENT_ID="
 gbp external-policy-update $APP_EXTERNAL_POLICY_ID --external-segments "$APP_EXTERNAL_SEGMENT_ID"
 
+# Launch the Eng tenant's stack
+set_user_password_tenant $NON_ADMIN_USERNAME $NON_ADMIN_PASSWORD $ENG_TENANT_NAME
+ 
+heat stack-create -f "$APP_YAML" "$ENG_STACK" -P "web_vm_image=$WEB_VM_IMAGE_NAME;web_vm_flavor=$WEB_VM_FLAVOR;app_vm_image=$APP_VM_IMAGE_NAME;app_vm_flavor=$APP_VM_FLAVOR;db_vm_image=$DB_VM_IMAGE_NAME;db_vm_flavor=$DB_VM_FLAVOR;mysql_rule_set_id=$MYSQL_RULE_SET_ID;app_rule_set_id=$APP_RULE_SET_ID;app_l3_policy_id=$APP_L3_POLICY_ID;mgmt_ptg_id=$MGMT_PTG_ID;monitoring_ip=$MONITORING_IP"
+
+confirm_resource_created "heat stack-show" "$ENG_STACK" "CREATE_COMPLETE"
+
+WEB_PTG_ID=`heat output-show "$ENG_STACK" "web_ptg_id" | sed "s/\"//g"`
+gbp group-update $WEB_PTG_ID --provided-policy-rule-sets "$HTTP_RULE_SET_ID=true"
+gbp group-update $WEB_PTG_ID --consumed-policy-rule-sets "$APP_TO_OUTSIDE_RULE_SET_ID=true"
+
+set_user_password_tenant $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_TENANT_NAME
+gbp external-policy-update $APP_EXTERNAL_POLICY_ID --consumed-policy-rule-sets "$HTTP_RULE_SET_ID=true"
+# Launching the Eng tenant's stack complete
+
+# Launch the HR tenant's stack
 set_user_password_tenant $NON_ADMIN_USERNAME $NON_ADMIN_PASSWORD $HR_TENANT_NAME
  
-heat stack-create -f "$APP_YAML" "$HR_STACK" -P "web_vm_image=$WEB_VM_IMAGE_NAME;web_vm_flavor=$WEB_VM_FLAVOR;app_vm_image=$APP_VM_IMAGE_NAME;app_vm_flavor=$APP_VM_FLAVOR;db_vm_image=$DB_VM_IMAGE_NAME;db_vm_flavor=$DB_VM_FLAVOR;mysql_rule_set_id=$MYSQL_RULE_SET_ID;app_rule_set_id=$APP_RULE_SET_ID;web_tier_consumed_prs_id=$APP_TO_OUTSIDE_RULE_SET_ID;web_tier_provided_prs_id=$HTTP_WITH_LB_REDIRECT_RULE_SET_ID;vip_ip_policy_id=$VIP_IP_POLICY_ID;app_l3_policy_id=$APP_L3_POLICY_ID;mgmt_ptg_id=$MGMT_PTG_ID;monitoring_ip=$MONITORING_IP"
+heat stack-create -f "$APP_YAML" "$HR_STACK" -P "web_vm_image=$WEB_VM_IMAGE_NAME;web_vm_flavor=$WEB_VM_FLAVOR;app_vm_image=$APP_VM_IMAGE_NAME;app_vm_flavor=$APP_VM_FLAVOR;db_vm_image=$DB_VM_IMAGE_NAME;db_vm_flavor=$DB_VM_FLAVOR;mysql_rule_set_id=$MYSQL_RULE_SET_ID;app_rule_set_id=$APP_RULE_SET_ID;app_l3_policy_id=$APP_L3_POLICY_ID;mgmt_ptg_id=$MGMT_PTG_ID;monitoring_ip=$MONITORING_IP"
 
 confirm_resource_created "heat stack-show" "$HR_STACK" "CREATE_COMPLETE"
 
 WEB_PTG_ID=`heat output-show "$HR_STACK" "web_ptg_id" | sed "s/\"//g"`
+gbp group-update $WEB_PTG_ID --network-service-policy "$VIP_IP_POLICY_ID"
 gbp group-update $WEB_PTG_ID --provided-policy-rule-sets "$HTTP_WITH_LB_REDIRECT_RULE_SET_ID=true"
+gbp group-update $WEB_PTG_ID --consumed-policy-rule-sets "$APP_TO_OUTSIDE_RULE_SET_ID=true"
 
 set_user_password_tenant $ADMIN_USERNAME $ADMIN_PASSWORD $ADMIN_TENANT_NAME
 gbp external-policy-update $APP_EXTERNAL_POLICY_ID --consumed-policy-rule-sets "$HTTP_WITH_LB_REDIRECT_RULE_SET_ID=true"
@@ -137,6 +134,7 @@ if [ "$RENDERING_MODE" == "$ACI_TRANSPARENT_SVC_RENDERING" ]; then
     gbp group-update $DB_PTG_ID --provided-policy-rule-sets "$MYSQL_VIA_FW_IDS_RULE_SET_ID=true"
     gbp group-update $APP_PTG_ID --consumed-policy-rule-sets "$MYSQL_VIA_FW_IDS_RULE_SET_ID=true"
 fi
+# Launching the HR tenant's stack complete
 
 
 echo "*********************************************************************"
