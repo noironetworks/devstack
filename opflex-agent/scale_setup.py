16ff367873cc9a573501365ca44d2196147a1c21
Noiro <noiro@vk8-1-u-26-1.noiro.lab>
2018-11-01 20:29:15 -0400

    Added networking setup for bridge, links to the bridge, namespace, dhclient. Needs testing and refinement.

--- opflex-agent/scale_setup.py
+++ opflex-agent/scale_setup.py
@@ -7,6 +7,8 @@ from logging.handlers import RotatingFileHandler
 import subprocess
 import sys
 import re
+from pybrctl import BridgeController
+from subprocess import PIPE
 
 
 class OpflexAgent:
@@ -51,26 +53,66 @@ class OpflexAgent:
 class NetworkSetup:
     def __init__(self, interface):
        self.interface = interface
+       self.brctl = BridgeController()
+       
 
     def isBridgePresent(self, bridge):
-       ret = subprocess.Popen( "brctl" + " show " +  bridge, shell=True, stdout=subprocess.PIPE)
-       for line in ret.stdout:
-          if bridge in line:
-             return bool(re.search('No such device', line))
+       for br in self.brctl.showall():
+          if 'br-agent'  in str(br):
+            return True
+       return False
+
            
     def setupBridge(self, bridge):
+       self.bridge = bridge
        if not self.isBridgePresent(bridge):
           try:
-             status = subprocess.call( "brctl" + " addbr " + bridge, shell=True, stdout=subprocess.PIPE) 
+             br = self.brctl.addbr("br-agent")
+             br.addif(self.interface)
           except OSError as e:
              logger.error("bridge creation failed\n" + e)
              raise
 
-    def createLink(self, name_space):
-       pass
+    def createLink(self, name_space, idx):
+       try:
+          subprocess.call("ip link add " + name_space + "-tap" + " type veth peer name tap", shell=True)
+          
+          mac = "00:01:00:00:00:" + format(idx, '02x')
+          subprocess.call("ip link set dev tap addr " + mac, shell=True)
+          subprocess.call("ip link set tap netns " + name_space, shell=True)
+          subprocess.call("brctl addif " + self.bridge + " " + name_space + "-tap", shell=True)
+       except:
+          logger.error("Unable to create link to namspace " + name_space)
+          raise
+       self.runDhclient(name_space, mac)
 
+    def runDhclient(self, name_space, mac):
+       # generate dhclient config from template.
+       dhclient_config_tmplt = Template('option rfc3442-classless-static-routes code 121 = array of unsigned integer 8;\n \
+option ms-classless-static-routes code 249 = array of unsigned integer 8;\n \
+option wpad code 252 = string;\n \
+interface "tap" {\n \
+\t\t\t\tsend host-name agent-$ns;\n \
+\t\t\t\tsend dhcp-client-identifier 01:$mac_addr;\n}\n')
+       dhclient_config = dhclient_config_tmplt.substitute( ns = name_space, mac_addr = mac )      
+       # config file needs to be in /etc/dhcp as dhclient executable cannot read any other location.
+       path_to_dhclient_file = '/etc/dhcp/dhclient-' + name_space + '.conf'
+       try:
+          f = open(path_to_dhclient_file, 'w')
+          f.write(dhclient_config)
+       except:
+          self.logger.error("Unable to write to config file " + path_to_dhclient_file + '\n' + sys.exc_info()[0])
+          raise
+       finally:
+          f.close()
+       # run dhclient to get ip lease
+       try:
+          p = subprocess.Popen("ip netns exec " +  name_space + " dhclient" " -cf " + path_to_dhclient_file + " -lf /var/lib/dhclient/dhclient-" + name_space + ".lease" + " -pf /var/run/dhclient-" + name_space + " tap" , shell=True, stdout=PIPE, stderr=PIPE)
+       except:
+          self.logger.error("Error starting DHCP client for " + name_space + '\n stdout:\n' + p.stdout + '\n stderr:\n' + p.stderr)
+          raise
 
-
+ 
 if __name__ == '__main__':
     # setup logging
     logging.basicConfig(level=logging.INFO)
@@ -108,6 +150,16 @@ if __name__ == '__main__':
 
     # setup the agent spawning loop
     for id in range(data["num_agents"]):
+        # create namespace and connect to bridge
+        try:
+            subprocess.call( "ip" + " netns" + " add" + " ns" + str(id+1), shell=True, stdout=subprocess.PIPE)
+        except:
+            e = sys.exc_info()[0]
+            logger.error("bridge creation failed\n" + e)
+            raise
+        # create links between namspace and bridge
+        nw.createLink("ns" + str(id+1), id+1)
+
         # setup listener socket dir for this agent
         socket_dir = '/etc/opflex-agent-ovs/' + str(id)
         if not os.path.exists(socket_dir):
